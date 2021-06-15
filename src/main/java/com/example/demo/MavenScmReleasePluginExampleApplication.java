24ed209c417a9139beb273c2b53520fe30e3e835
keshabdora <kesabdora333@gmail.com>
2021-06-15 19:45:23 +0530

    new comment added in changes

--- src/main/java/com/example/demo/MavenScmReleasePluginExampleApplication.java
+++ src/main/java/com/example/demo/MavenScmReleasePluginExampleApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MavenScmReleasePluginExampleApplication {
-
+/**
+ * added new comment
+ * @param args
+ */
 	public static void main(String[] args) {
 		SpringApplication.run(MavenScmReleasePluginExampleApplication.class, args);
 	}
